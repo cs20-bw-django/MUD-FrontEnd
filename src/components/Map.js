b76e61b23b854a9159b41877c96d22fb73204fc9
ItelSunday <iteldomingotech@gmail.com>
2019-08-29 23:22:50 -0500

    updated navigation, buttons working: check endpoint

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
+import up from '../assets/up.png';
+import down from '../assets/down.png';
+import left from '../assets/left.png';
+import right from '../assets/right.png';
+
 
 export default class Map extends Component {
     constructor(props) {
@@ -8,20 +13,38 @@ export default class Map extends Component {
     
         this.state = {
              rooms: [],
+             newRoom: []
              
         }
 
     }
 
     getRooms = () => {
+        axios
+        .get('https://csbw-1.herokuapp.com/api/adv/draw')
+        .then(res=>{
+            this.setState({
+                rooms: res.data 
+            })
+        })
+        .catch(err => {
+            console.log(err.response);
+          });
 
     }
 
     navigate = (str) => {
         axios
-        .post('url', str)
-        .then()
-        .catch()
+        .post('https://csbw-1.herokuapp.com/api/adv/move', str)
+        .then(res=>{
+            this.setState({
+                newRoom: res.data
+            })
+
+        })
+        .catch(err => {
+            console.log(err.response);
+          });
     }
 
     componentDidMount() {
@@ -49,10 +72,10 @@ export default class Map extends Component {
         return (
             <div>
                 <div>
-                    <button onClick={() => this.navigate("n")}>up</button>
-                    <button onClick={() => this.navigate("s")}>down</button>
-                    <button onClick={() => this.navigate("w")}>left</button>
-                    <button onClick={() => this.navigate("e")}>right</button>
+                    <button onClick={() => this.navigate("n")}><img src={up} alt="up"/></button>
+                    <button onClick={() => this.navigate("n")}><img src={down} alt="down"/></button>
+                    <button onClick={() => this.navigate("n")}><img src={left} alt="left"/></button>
+                    <button onClick={() => this.navigate("n")}><img src={right} alt="right"/></button>
                 </div>
             </div>
         )
